0be6bda265a3c0b6c5902d7954a49fa13d424526
Amit Jotwani <hello@ajot.me>
2019-09-04 13:32:00 -0400

    Wrapped up Day 1
    https://www.hackingwithswift.com/100/1

--- Day-1/MyPlayground.playground/Contents.swift
+++ Day-1/MyPlayground.playground/Contents.swift
@@ -3,4 +3,39 @@ import UIKit
 var str = "Hello Playground"
 str = "Goodbye world"
 
+//Integers
+var age = 30
 
+//Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles and booleans
+
+var pi = 3.141
+
+var awesome = true
+
+//String Interpolation
+var score = 85
+var str_score = "Your score was \(score)"
+
+//constants
+
+let taylor = "swift"
+
+//annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
